b7230f128c35c76ee1b3e981514860ed15ae32a2
HectorMtz22 <hec.gre81@gmail.com>
2022-01-05 00:23:05 -0600

    Make it look better

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import "./App.css";
 function App() {
   return (
     <>
-      <article>Título #</article>
-      <article>Título #</article>
-      <article>Título #</article>
-      <article>Título #</article>
-      <article>Título #</article>
+      <article>
+        <h1>Título 1</h1>
+      </article>
+      <article>
+        <h1>Título 2</h1>
+      </article>
+      <article>
+        <h1>Título 3</h1>
+      </article>
+      <article>
+        <h1>Título 4</h1>
+      </article>
+      <article>
+        <h1>Título 5</h1>
+      </article>
     </>
   );
 }
